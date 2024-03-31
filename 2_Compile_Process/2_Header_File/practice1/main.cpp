f00e6c28f7c0be20fe4b9e4f4b21d0d5d44201c4
JongHoon <qnm541@icloud.com>
2024-03-31 16:37:45 +0900

    Compile Process - Header File

---
+++ 2_Compile_Process/2_Header_File/practice1/main.cpp
@@ -0,0 +1,21 @@
+#include "foo.h"
+
+int main()
+{
+    foo();
+    return 0;
+}
+
+/*
+링킹 에러   
+
+Undefined symbols for architecture arm64:
+  "foo()", referenced from:
+      _main in main-80f16b.o
+ld: symbol(s) not found for architecture arm64
+clang: error: linker command failed with exit code 1 (use -v to see invocation)
+*/
+
+// g++ main.cpp -o foo -Wall -Werror - object 파일 생성, 머신코드
+
+// g++ main.cpp -S -Wall -Werror - assembly 파일 
---
+++ 2_Compile_Process/2_Header_File/practice2/main.cpp
@@ -0,0 +1,8 @@
+#include "cat.h"
+
+int main()
+{
+    Cat kitty;
+    kitty.speak();
+    return 0;
+} 
